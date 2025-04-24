b8a0ca313a15638ba9c4bacd6a791716489f3a72
Shabarish-P-Sujith <shabarishpsujith@gmail.com>
2025-04-24 13:59:28 +0530

    added Database

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: DB_NAME,
+            dbName: process.env.DB_NAME,
         });
         console.log(`MongoDB Connection Successful !!`);
     } catch (error) {
