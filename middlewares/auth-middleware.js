70271c472ed93378150f085efaa39e3eb828c163
Shabarish-P-Sujith <shabarishpsujith@gmail.com>
2025-04-25 23:22:18 +0530

    added Middleware for AUTH purposes

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    console.log("AuthMiddleware has been called !!!");
+
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+
+    const token = authHeader && authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: "Access Denied.. No Token is provided !!",
+        })
+    }
+
+    // Decode the Token
+    try {
+        var decodedTokenInfo = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        console.log(decodedTokenInfo);
+
+        req.userInfo = decodedTokenInfo; // Decoded token is STORED so the next middleware or route can use it
+        next(); // now this will call the "/welcome" route, authentication
+
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Access Denied.. No Token is provided !!",
+        })
+    }
+
+    next();
+}
+module.exports = authMiddleware;
