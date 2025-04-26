0c4ca0b41fa54c47ccb44704b23d6a45d013cc70
Shabarish-P-Sujith <shabarishpsujith@gmail.com>
2025-04-26 13:00:00 +0530

    added Admin route

---
+++ routes/admin-routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/auth-middleware');
+const adminMiddleware = require('../middlewares/admin-middleware');
+
+router.get('/welcome', authMiddleware, adminMiddleware, (req, res) => {
+    res.json({
+        message: "Welcome to the Admin Page",
+    });
+})
+
+module.exports = router;
